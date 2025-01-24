6b0d99032241ea4aae8ff6ed1b4955cfbf3b572e
g0del-esch3r-bach <eddylihere@gmail.com>
2025-01-24 13:17:45 -0800

    expected value

--- 2.9/2.9.cpp
+++ 2.9/2.9.cpp
@@ -13,7 +13,7 @@ void simul(int N)
     int B[][2][2]={{{0,0},{0,0}},{{0,0},{0,0}}};
     while(res<8){
         V.push_back(rand()%2);
-        cout<<V[ctr];
+        //cout<<V[ctr];
         ctr+=1;
         if (ctr<3){
             continue;
@@ -23,12 +23,12 @@ void simul(int N)
             res+=1;
         }
     }
-    cout<<endl<<endl;
+    //cout<<endl<<endl;
     for (i=0;i<=1;i=i+1){
         for (j=0;j<=1;j=j+1){
             for (k=0;k<=1;k=k+1){
                 A[i][j][k][N]=B[i][j][k];
-                cout<<i<<j<<k<<" "<<B[i][j][k]<<endl;
+                //cout<<i<<j<<k<<" "<<B[i][j][k]<<endl;
             }
         }
     }
@@ -39,19 +39,29 @@ int main()
     freopen("game.out","w",stdout);
     srand(time(0));
     int N,i,j,k,l;
+    float E[2][2][2];
     cin>>N;
     for (i=0;i<=N-1;i=i+1){
         simul(i);
-        cout<<endl<<"================================"<<endl<<endl;
+        //cout<<endl<<"================================"<<endl<<endl;
     }
     for (i=0;i<=1;i=i+1){
         for (j=0;j<=1;j=j+1){
             for (k=0;k<=1;k=k+1){
-                cout<<i<<j<<k<<" ";
+                E[i][j][k]=0;
+                //cout<<i<<j<<k<<endl;
                 for (l=0;l<=N-1;l=l+1){
-                    cout<<A[i][j][k][l]<<" ";
+                    //cout<<A[i][j][k][l]<<endl;
+                    E[i][j][k]+=(float)A[i][j][k][l]/(float)N;
                 }
-                cout<<endl;
+                //cout<<endl;
+            }
+        }
+    }
+    for (i=0;i<=1;i=i+1){
+        for (j=0;j<=1;j=j+1){
+            for (k=0;k<=1;k=k+1){
+                cout<<i<<j<<k<<" "<<E[i][j][k]<<endl;
             }
         }
     }
