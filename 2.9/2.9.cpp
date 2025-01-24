be31ee7e742cf54e99d868ad5b84a3eafcc2c255
g0del-esch3r-bach <eddylihere@gmail.com>
2025-01-24 13:35:03 -0800

    nvm dumb

--- 2.9/2.9.cpp
+++ 2.9/2.9.cpp
@@ -39,7 +39,7 @@ int main()
     freopen("game.out","w",stdout);
     srand(time(0));
     int N,i,j,k,l,temp[3];
-    float E[2][2][2],W[2][2][2];
+    float E[2][2][2],P[2][2][2];
     cin>>N;
     for (i=0;i<=N-1;i=i+1){
         simul(i);
@@ -49,7 +49,7 @@ int main()
         for (j=0;j<=1;j=j+1){
             for (k=0;k<=1;k=k+1){
                 E[i][j][k]=0;
-                W[i][j][k]=0;
+                P[i][j][k]=0;
                 //cout<<i<<j<<k<<endl;
                 for (l=0;l<=N-1;l=l+1){
                     //cout<<A[i][j][k][l]<<endl;
@@ -59,7 +59,7 @@ int main()
             }
         }
     }
-    for (l=0;l<=N-1;l=l+1){
+    /*for (l=0;l<=N-1;l=l+1){
         temp[0]=0;
         temp[1]=0;
         temp[2]=0;
@@ -75,13 +75,14 @@ int main()
                 }
             }
         }
-        W[temp[0]][temp[1]][temp[2]]+=(float)1/(float)N;
-    }
+        //cout<<temp[0]<<" "<<temp[1]<<" "<<temp[2]<<" winner round"<<N<<endl;
+        P[temp[0]][temp[1]][temp[2]]+=(float)1/(float)N;
+    }*/
     for (i=0;i<=1;i=i+1){
         for (j=0;j<=1;j=j+1){
             for (k=0;k<=1;k=k+1){
                 cout<<"E[flips --> "<<i<<j<<k<<"] = "<<E[i][j][k]<<endl;
-                cout<<"P[win "<<i<<j<<k<<"] = "<<W[i][j][k]<<endl;
+                //cout<<"P[win "<<i<<j<<k<<"] = "<<P[i][j][k]<<endl;
             }
         }
     }
