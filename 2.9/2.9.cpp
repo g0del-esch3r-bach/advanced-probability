53642ce29bfe3f8c4f8b0a633b068d1726c6f259
g0del-esch3r-bach <eddylihere@gmail.com>
2025-01-24 12:14:42 -0800

    simul x1000

--- 2.9/2.9.cpp
+++ 2.9/2.9.cpp
@@ -3,14 +3,15 @@
 #include<ctime>
 #include<vector>
 using namespace std;
-int main()
+int A[2][2][2][10000];
+void simul(int N)
 {
     srand(time(0));
     vector<int>V;
     int res,ctr,i,j,k;
     res=0;
     ctr=0;
-    int A[][2][2]={{{0,0},{0,0}},{{0,0},{0,0}}};
+    int B[][2][2]={{{0,0},{0,0}},{{0,0},{0,0}}};
     while(res<8){
         V.push_back(rand()%2);
         cout<<V[ctr];
@@ -18,8 +19,8 @@ int main()
         if (ctr<3){
             continue;
         }
-        if (A[V[ctr-3]][V[ctr-2]][V[ctr-1]]==0){
-            A[V[ctr-3]][V[ctr-2]][V[ctr-1]]=ctr;
+        if (B[V[ctr-3]][V[ctr-2]][V[ctr-1]]==0){
+            B[V[ctr-3]][V[ctr-2]][V[ctr-1]]=ctr;
             res+=1;
         }
     }
@@ -27,9 +28,18 @@ int main()
     for (i=0;i<=1;i=i+1){
         for (j=0;j<=1;j=j+1){
             for (k=0;k<=1;k=k+1){
-                cout<<i<<j<<k<<" "<<A[i][j][k]<<endl;
+                A[i][j][k][N]=B[i][j][k];
+                cout<<i<<j<<k<<" "<<B[i][j][k]<<endl;
             }
         }
     }
-    return 0;
+    return;
+}
+int main()
+{
+    for (int i=0;i<=1000;i=i+1)
+    {
+        simul(i);
+        cout<<endl<<"================================"<<endl;
+    }
 }
