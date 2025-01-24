16304be0da419fe72b05512959da157d389733ec
g0del-esch3r-bach <eddylihere@gmail.com>
2025-01-24 11:58:24 -0800

    seed()

--- 2.9/2.9.cpp
+++ 2.9/2.9.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<cstdlib>
+#include<ctime>
 using namespace std;
 int main()
 {
+    srand(time(0));
     int i;
-    for (i=0;i<=25;i=i+1)
+    for (i=0;i<=42;i=i+1)
     {
         cout<<rand()%2;
     }
