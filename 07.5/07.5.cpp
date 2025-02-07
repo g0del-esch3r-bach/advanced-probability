12ad65b47b6e6955863854aaeb8dcb766bbed4a5
g0del-esch3r-bach <eddylihere@gmail.com>
2025-02-06 22:48:26 -0800

    simul

--- 07.5/07.5.cpp
+++ 07.5/07.5.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 int simul(int N,int M)
 {
-    int str,fin,prz,i;
+    int str,fin,prz,temp,i;
     set<int>open,shut;
     vector<int>V;
     for (i=1;i<=N;i=i+1){
@@ -14,15 +14,31 @@ int simul(int N,int M)
     }
     str=rand()%N+1;
     prz=rand()%N+1;
+    shut.erase(str);
     shut.erase(prz);
-    for (i=0;i<=M-1;i=i+1)
-    {
+    for (i=0;i<=M;i=i+1){
+        V.clear();
         copy(shut.begin(),shut.end(),back_inserter(V));
+        if(i<M){
+            temp=V[rand()%V.size()];
+            shut.erase(temp);
+            open.insert(temp);
+        }
+        else{
+            V.push_back(prz);
+            fin=V[rand()%V.size()];
+        }
     }
+    cout<<"init guess: "<<str<<endl<<"monty opens: ";
+    for (i : open){
+        cout<<i<<" ";
+    }
+    cout<<endl<<"final guess: "<<fin<<endl<<"prize door: "<<prz<<endl<<endl;
+    return (fin==prz);
 }
 int main()
 {
-    int N,M,T;
+    int N,M,T,i;
     cout<<"# total doors:";
     cin>>N;
     cout<<"# goats revealed:";
@@ -30,5 +46,8 @@ int main()
     cout<<"# iterations";
     cin>>T;
     srand(time(0));
+    for (i=0;i<=T-1;i=i+1){
+        cout<<simul(N,M);
+    }
     return 0;
 }
