ccc271a0eed547fd23b148a2559fc0965ff4131a
g0del-esch3r-bach <eddylihere@gmail.com>
2025-02-06 21:54:18 -0800

    adding stuff

--- 07.5/07.5.cpp
+++ 07.5/07.5.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<set>
+#include<vector>
 using namespace std;
+int simul(int N,int M)
+{
+    int str,fin,prz,i;
+    set<int>open,shut;
+    for (i=1;i<=N;i=i+1){
+        shut.insert(i);
+    }
+    str=rand()%N+1;
+    prz=rand()%N+1;
+    shut.erase(prz);
+    for (i=0;i<=M-1;i=i+1)
+    {
+        
+    }
+}
 int main()
 {
-    int N,M;
-    cin>>N>>M;
+    int N,M,T;
+    cout<<"# total doors:";
+    cin>>N;
+    cout<<"# goats revealed:";
+    cin>>M;
+    cout<<"# iterations";
+    cin>>T;
+    srand(time(0));
     return 0;
 }
